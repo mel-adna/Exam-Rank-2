c579f4bbe1a6ddf17c282ed62a5ebf4fd8b56f5a
Mohamed El adnani <mohamedeladnani0@gmail.com>
2024-12-31 13:47:15 +0100

    update

---
+++ repeat_alpha.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+int main(int arc, char *arv[])
+{
+	if (arc == 2 && arv[1][0] != '\0')
+	{
+		int i = 0;
+		while (arv[1][i])
+		{
+			
+			
+		}
+		
+	}
+	write(1, "\n", 1);
+}
